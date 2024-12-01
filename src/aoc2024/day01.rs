dae27142101e2140d313f4459bafa51a4c48354a
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-12-01 19:31:16 +0100

    Add Day01 part2

--- src/aoc2024/day01.rs
+++ src/aoc2024/day01.rs
@@ -23,3 +23,24 @@ pub fn part01(file_path: &str) -> usize {
 
     distances.sum()
 }
+
+pub fn part02(file_path: &str) -> usize {
+    let puzzle_input: String = std::fs::read_to_string(file_path).unwrap();
+    let lists = puzzle_input.lines().map(|line| {
+        let mut line = line.trim().split("   ");
+        let lv = line.next().unwrap().trim().parse::<u32>().unwrap();
+        let rv = line.next().unwrap().trim().parse::<u32>().unwrap();
+
+        (lv, rv)
+    });
+
+    let (left, right): (Vec<u32>, Vec<u32>) = lists.unzip();
+
+    let mut similarity_score = 0;
+    for l in left.iter() {
+        let similarity = right.iter().filter(|x| **x == *l).count();
+        similarity_score += (*l as usize) * similarity;
+    }
+
+    similarity_score
+}
