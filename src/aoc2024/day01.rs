852943999a2835449ec649deb00fa963d04dfb59
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-12-01 19:27:08 +0100

    Add Day01 part1

---
+++ src/aoc2024/day01.rs
@@ -0,0 +1,25 @@
+pub fn part01(file_path: &str) -> usize {
+    let puzzle_input: String = std::fs::read_to_string(file_path).unwrap();
+    let lists = puzzle_input.lines().map(|line| {
+        let mut line = line.trim().split("   ");
+        let lv = line.next().unwrap().trim().parse::<u32>().unwrap();
+        let rv = line.next().unwrap().trim().parse::<u32>().unwrap();
+
+        (lv, rv)
+    });
+
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = lists.unzip();
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let distances = left.into_iter().zip(right.into_iter()).map(|(l, r)| {
+        let l = l as i32;
+        let r = r as i32;
+        let d = i32::abs(r - l);
+
+        let d: usize = d.try_into().unwrap();
+        d
+    });
+
+    distances.sum()
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use aoc2024_rust::aoc2024;
+
+fn main() {
+    println!("\tDay 01: ");
+    let day01_part1 = aoc2024::day01::part01("./puzzle_input/day01.txt");
+    println!("\t\tpart 1: {day01_part1}");
+}
